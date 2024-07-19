a03e38a320de8e506ea2b13ddb981992925d275d
Manish Aryan <93929653+manish7065@users.noreply.github.com>
2024-07-19 15:56:02 +0530

    updates

--- resources/leetcode.sql
+++ resources/leetcode.sql
@@ -177,3 +177,56 @@ WHERE allergies NOT NULL
 GROUP BY allergies
 ORDER BY count(*) DESC
 
+-- Show all of the patients grouped into weight groups.
+-- Show the total amount of patients in each weight group.
+-- Order the list by the weight group decending.
+
+-- For example, if they weight 100 to 109 they are placed in the 100 weight group, 110-119 = 110 weight group, etc.
+
+-- self soln
+
+select
+  floor(weight / 10) * 10 as weight_group,
+  count(*)
+from patients
+group by weight_group
+order by weight_group desc;
+
+-- suggested:
+select
+  floor(weight / 10) * 10 as weight_group,
+  count(*)
+from patients
+group by weight_group
+order by weight_group desc;
+
+SELECT
+  TRUNCATE(weight, -1) AS weight_group,
+  count(*)
+FROM patients
+GROUP BY weight_group
+ORDER BY weight_group DESC;
+
+
+-- Show patient_id, weight, height, isObese from the patients table.
+-- Display isObese as a boolean 0 or 1.
+-- Obese is defined as weight(kg)/(height(m)2) >= 30.
+-- weight is in units kg.
+-- height is in units cm.
+
+select
+  patient_id,
+  weight,
+  height,
+  case
+    when (weight / ((height / 100.0) * (height / 100.0))) >= 30 then 1
+    else 0
+  end as isObese
+from patients
+
+SELECT
+  patient_id,
+  weight,
+  height,
+  weight / power(CAST(height AS float) / 100, 2) >= 30 AS obese
+FROM patients
